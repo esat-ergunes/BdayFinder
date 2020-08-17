b73bf02f02820332013af373618c4d17a37de14d
Esat <r0765162@student.thomasmore.be>
2020-08-17 07:06:09 +0300

    HTML&CSS: layout of the signup page has been changed and an alert banner has been added in case of error.

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -2,7 +2,7 @@
 //const passport = require("passport");
 //const { json } = require("express");
 
-let btnSignup = document.querySelector(".signup button").addEventListener("click",()=>{
+let btnSignup = document.querySelector(".SignUp").addEventListener("click",()=>{
 
     let username = document.querySelector("#email").value;
     let password = document.querySelector("#password").value;
@@ -21,15 +21,29 @@ let btnSignup = document.querySelector(".signup button").addEventListener("click
     }).then(json=>{
         if(json.status === "success"){
             let feedback = document.querySelector(".alert");
-            feedback.textContent="Sign up complete!";
+            feedback.textContent="✅ Sign up complete!";
             feedback.classList.remove('hidden');
+            feedback.classList.add("alert-success");
             let token = json.data.token;
 
             localStorage.setItem("token",token);
             window.location.href="app.html";
 
         }
+       
+        
+       
     });
-
+ if (username || password === "") {
+   console.log("all fileds are required!");
+   let feedback = document.querySelector(".alert");
+   feedback.textContent = "😱 Oops, you need to complete all fields to register.";
+    feedback.classList.remove("hidden");
+    setTimeout(() => {
+      document.querySelector(".alert").classList.add("hidden");
+    }, 5000);
+    feedback.classList.add("alert-danger");
+ }
+   
 
 });
