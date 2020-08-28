a25290bc8043e0b707cada41e251829bb4c09e9e
Esat <r0765162@student.thomasmore.be>
2020-08-28 14:53:03 +0200

    Js: attempt10  to fix netlify & Heroku

--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,14 @@ const authController = require('../controllers/auth');
 const userController = require('../controllers/api/v1/userData');
 const passport = require('passport');
 const { authenticate } = require('passport');
-const base_url = "https://bdayfinder.herokuapp.com";
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post(base_url  + "/signup", authController.signup);
-router.post(base_url + "/login", authController.login);
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
 
 
 
