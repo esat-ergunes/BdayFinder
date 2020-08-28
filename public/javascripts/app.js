0091346504a01b25e8032e20f1b238d580f1c2ae
Esat <r0765162@student.thomasmore.be>
2020-08-28 15:20:23 +0200

    Js: attempt14  to fix netlify & Heroku

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,7 +4,7 @@ if (!localStorage.getItem("token")) {
   window.location.href = "login.html";
 }
 
- fetch(base_url + "/api/v1/todos", {
+ fetch(base_url + "/api/v1/userData", {
    'headers': {
      'Authorization': 'Bearer ' + localStorage.getItem('token')
    }
