eb401acf4fa13f0d57d802ad7bba6cd932e97ea2
Esat <r0765162@student.thomasmore.be>
2020-08-29 06:20:50 +0200

    Primus: attempt23 for primus.

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,6 +1,6 @@
 const base_url = "https://bdayfinder.herokuapp.com";
 
-primus = Primus.connect(base_url, {
+primus = Primus.connect("http://localhost:3000", {
   reconnect: {
       max: Infinity // Number: The max delay before we try to reconnect.
     , min: 500 // Number: The minimum delay before we try reconnect.
