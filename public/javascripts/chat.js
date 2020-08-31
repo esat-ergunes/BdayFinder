64e1202454a6afcccecce5face1626269913bc08
Esat <r0765162@student.thomasmore.be>
2020-08-31 05:10:23 +0200

    JS: Trying to scroll down when a user send a message attempt6

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -70,7 +70,7 @@ btnSend.addEventListener('click', (e)=>{
            //appendMessage(json);
            document.querySelector(".msgList").animate({scrollTop:10000000});
            let objDiv = document.querySelector(".msgList");
-               objDiv.scrollTop = objDiv.scrollHeight + 204 ;
+               objDiv.scrollTop = objDiv.scrollHeight + 10000000 ;
         }).catch(err =>{
             console.log(err);
         })
